dfacf9d9b1b23518e5d4fee2808adf463f529b18
Lucas Kenji Uezu <kenjiuezu3@gmail.com>
2024-08-08 23:07:41 -0300

    Implementar classes Iphone, Nokia e Smartphone e teste

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(
+    numero: "123123",
+    modelo: "Model 1",
+    imei: "123123123123123",
+    memoria: 64
+);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\nSmartphone Nokia:");
+Smartphone iphone = new Iphone(
+    numero: "789789",
+    modelo: "Model 2",
+    imei: "789789789789789",
+    memoria: 128
+);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitter");
+
